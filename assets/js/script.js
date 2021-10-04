4806860ec8cb1134d1673d7ec7c668ccb23baf09
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-10-04 10:45:20 -0400

    Got multi markers showing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,17 @@ var cuisineType = "";
 var healthType = "";
 
 function generateRecipes() {
-
-// captures drop down inputs and joins them with & to fit API needs
-  var cuisineType = $("#cuisine_chosen .result-selected") 
-    .map(function() {
+  // captures drop down inputs and joins them with & to fit API needs
+  var cuisineType = $("#cuisine_chosen .result-selected")
+    .map(function () {
       return "cuisineType=" + this.innerHTML;
     })
     .get()
     .join("&");
-  console.log(cuisineType); 
+  console.log(cuisineType);
 
   var healthType = $("#health_chosen .result-selected")
-    .map(function() {
+    .map(function () {
       return "healthType=" + this.innerHTML;
     })
     .get()
@@ -24,17 +23,17 @@ function generateRecipes() {
   var ingredients = document.querySelector("#ingredient").value;
   console.log(ingredients);
   //API URL with ingredients input added
-  var apiURL = "https://api.edamam.com/api/recipes/v2?type=public&q=" 
-  + ingredients 
-  + "&app_id=d79a6135&app_key=%20640f82c48f99da84b3fcbf2e006bd954&ingr=1-10&diet=balanced&health=gluten-free&health=tree-nut-free&"
-  + cuisineType
-  + "&time=0-20&imageSize=REGULAR&random=true&field=uri&field=label&field=image&field=source&field=url&field=yield&field=dietLabels&field=healthLabels&field=cautions&field=ingredientLines&field=ingredients&field=calories&field=totalWeight&field=totalTime&field=cuisineType&field=mealType&field=dishType&field=totalNutrients&field=totalDaily"
+  var apiURL =
+    "https://api.edamam.com/api/recipes/v2?type=public&q=" +
+    ingredients +
+    "&app_id=d79a6135&app_key=%20640f82c48f99da84b3fcbf2e006bd954&ingr=1-10&diet=balanced&health=gluten-free&health=tree-nut-free&" +
+    cuisineType +
+    "&time=0-20&imageSize=REGULAR&random=true&field=uri&field=label&field=image&field=source&field=url&field=yield&field=dietLabels&field=healthLabels&field=cautions&field=ingredientLines&field=ingredients&field=calories&field=totalWeight&field=totalTime&field=cuisineType&field=mealType&field=dishType&field=totalNutrients&field=totalDaily";
 
-    fetch(apiURL)
-    .then(function(response) {
-      //request was successful
-      if (response.ok) {
-        response.json().then(function(data) {
+  fetch(apiURL).then(function (response) {
+    //request was successful
+    if (response.ok) {
+      response.json().then(function (data) {
         console.log(data);
 
         //grabbing where the recipe cards will go, and setting it to empty to clear if repeat searches are done
@@ -42,27 +41,33 @@ function generateRecipes() {
         recipeContainer.innerHTML = "";
 
         // for loop to populate 3 recipe cards with info captured from the API
-        for (var i = 0; i <=2; i++) {
+        for (var i = 0; i <= 2; i++) {
           var recipeEl = document.createElement("div");
           recipeEl.classList = "text-center";
           recipeEl.setAttribute("id", "generate");
           recipeEl.innerHTML = data.hits[i].recipe.label + "<br />";
           console.log(data.hits[i].recipe.label);
-          recipeEl.innerHTML+= "Serves: " + data.hits[i].recipe.yield +  "<br />";
-          recipeEl.innerHTML+= '<img src="' + data.hits[i].recipe.image + '"> <br />';
-          recipeEl.innerHTML+= "Ingrediets: " + data.hits[i].recipe.ingredientLines +  "<br /> <br />";
-          recipeEl.innerHTML+= "<a href='" + data.hits[i].recipe.url + "' target='_blank'>Click here to go to the recipe!</a> <br />";
+          recipeEl.innerHTML +=
+            "Serves: " + data.hits[i].recipe.yield + "<br />";
+          recipeEl.innerHTML +=
+            '<img src="' + data.hits[i].recipe.image + '"> <br />';
+          recipeEl.innerHTML +=
+            "Ingrediets: " +
+            data.hits[i].recipe.ingredientLines +
+            "<br /> <br />";
+          recipeEl.innerHTML +=
+            "<a href='" +
+            data.hits[i].recipe.url +
+            "' target='_blank'>Click here to go to the recipe!</a> <br />";
           recipeContainer.appendChild(recipeEl);
-
-        };
-        });
-      };
-    });
+        }
+      });
+    }
+  });
 }
 
-
 // This is for the dropdown plug in to get them to work
-$(function() {
+$(function () {
   $(".chosen-select").chosen({
     max_selected_options: 3,
     width: "90%",
@@ -82,60 +87,191 @@ const map = new mapboxgl.Map({
 // Add the control to the map.
 map.addControl(
   new MapboxGeocoder({
-  accessToken: mapboxgl.accessToken,
-  mapboxgl: mapboxgl
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
   })
-  );
+);
 
-var shopLong = " "
-var shopLat = " "
-var shopTitle = " "
-var shopAddress = " " 
+var shopLong = " ";
+var shopLat = " ";
+var shopTitle = " ";
+var shopAddress = " ";
 //Map API
-var alsoApiURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/market%2Cbutcher.json?access_token=pk.eyJ1IjoiYXp1cnl0ZSIsImEiOiJja3U5dGpsbjYwYTZnMnZubmVtZXZ4bzcyIn0.4TNon4_EuBE9I_4Xg-U7kQ&cachebuster=1633231166631&autocomplete=false&types=poi&bbox=-76.2%2C45.0%2C-75.2%2C45.6&limit=10"
-  fetch(alsoApiURL)
-  .then(function(response) {
-    //request was successful
-    if (response.ok) {
-      return response.json().then(function(response) {
-      console.log(response) 
-      console.log("Long,Lat is " + response.features[0].center[0] + " " + response.features[0].center[1] )
-      shopLong= response.features[0].center[0]
-      shopLat = response.features[0].center[1]
-      console.log("Store Name: " + response.features[0].text)
-      shopTitle=response.features[0].text
-      console.log("Store Address: " + response.features[0].properties.address)
-      shopAddress= response.features[0].properties.address
-    
+var alsoApiURL =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/market%2Cbutcher.json?access_token=pk.eyJ1IjoiYXp1cnl0ZSIsImEiOiJja3U5dGpsbjYwYTZnMnZubmVtZXZ4bzcyIn0.4TNon4_EuBE9I_4Xg-U7kQ&cachebuster=1633231166631&autocomplete=false&types=poi&bbox=-76.2%2C45.0%2C-75.2%2C45.6&limit=10";
+fetch(alsoApiURL).then(function (response) {
+  //request was successful
+  if (response.ok) {
+    return response.json().then(function (response) {
+      console.log(response);
+      console.log(
+        "Long,Lat is " +
+          response.features[0].center[0] +
+          " " +
+          response.features[0].center[1]
+      );
+      shopLong = response.features[0].center[0];
+      shopLat = response.features[0].center[1];
+      console.log("Store Name: " + response.features[0].text);
+      shopTitle = response.features[0].text;
+      console.log("Store Address: " + response.features[0].properties.address);
+      shopAddress = response.features[0].properties.address;
+      // var marker = [];  
+      // for (var i = 0; i < response.features.length; i++) {
+      //   var builder = [
+      //     {
+      //       type: "Feature",
+      //       geometry: {
+      //         type: "Point",
+      //         coordinates: [
+      //           response.features[i].center[0],
+      //           response.features[i].center[1]],
+      //       },
+      //       properties: {
+      //         title: response.features[i].text,
+      //         description: response.features[i].properties.address,
+      //       },
+      //     },
+      //   ];
+      //   marker.push(builder);
+      // }
+      // console.log(marker);
+      // console.log((response.features[0].center[0]))
+
+ 
       const geojson = {
         type: 'FeatureCollection',
         features: [
           {
             type: 'Feature',
             geometry: {
-              type: 'Point',
-              coordinates: [shopLong, shopLat]
+              type: "Point",
+              coordinates: [response.features[0].center[0], response.features[0].center[1]]
             },
             properties: {
-              title: shopTitle,
-              description: shopAddress
+              title: response.features[0].text,
+              description: response.features[0].properties.address,
             }
-          }
-        ]
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [response.features[1].center[0], response.features[1].center[1]]
+            },
+            properties: {
+              title: response.features[1].text,
+              description: response.features[1].properties.address,
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [response.features[2].center[0], response.features[2].center[1]]
+            },
+            properties: {
+              title: response.features[2].text,
+              description: response.features[2].properties.address,
+            }
+            },
+            {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [response.features[3].center[0], response.features[3].center[1]]
+            },
+            properties: {
+              title: response.features[3].text,
+              description: response.features[3].properties.address,
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [response.features[4].center[0], response.features[4].center[1]]
+            },
+            properties: {
+              title: response.features[4].text,
+              description: response.features[4].properties.address,
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [response.features[5].center[0], response.features[5].center[1]]
+            },
+            properties: {
+              title: response.features[5].text,
+              description: response.features[5].properties.address,
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [response.features[6].center[0], response.features[6].center[1]]
+            },
+            properties: {
+              title: response.features[6].text,
+              description: response.features[6].properties.address,
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [response.features[7].center[0], response.features[7].center[1]]
+            },
+            properties: {
+              title: response.features[7].text,
+              description: response.features[7].properties.address,
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [response.features[8].center[0], response.features[8].center[1]]
+            },
+            properties: {
+              title: response.features[8].text,
+              description: response.features[8].properties.address,
+            }
+          },
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [response.features[9].center[0], response.features[9].center[1]]
+            },
+            properties: {
+              title: response.features[9].text,
+              description: response.features[9].properties.address,
+            },
+          },
+        ],
       };
 
       // add markers to map
-for (const { geometry, properties } of geojson.features) {
-  // create a HTML element for each feature
-  const el = document.createElement('div');
-  el.className = 'marker';
+      for (const { geometry, properties } of geojson.features) {
+        // create a HTML element for each feature
+        const el = document.createElement("div");
+        el.className = "marker";
 
-  // make a marker for each feature and add to the map
-  new mapboxgl.Marker(el).setLngLat(geometry.coordinates)
-  .setPopup(
-    new mapboxgl.Popup({ offset: 25 }) // add popups
-      .setHTML(`<h3>${properties.title}</h3><p>${properties.description}</p>`)
-  )
-  .addTo(map);
-}
-})}})
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker(el)
+          .setLngLat(geometry.coordinates)
+          .setPopup(
+            new mapboxgl.Popup({ offset: 25 }) // add popups
+              .setHTML(
+                `<h3>${properties.title}</h3><p>${properties.description}</p>`
+              )
+          )
+          .addTo(map);
+      }
+    });
+  }
+});
